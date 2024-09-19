83726ac821f27fee93209792cf69e549909c6445
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-19 08:02:52 +0700

    praktikum 1 tampilan echo terpisah poin 8

--- minggu4/variabel_konstanta.php
+++ minggu4/variabel_konstanta.php
@@ -2,16 +2,16 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasi = $angka1 +$angka2;
-echo"Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. ";
+echo"Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br> ";
 
 $benar = true;
 $salah = false;
-echo "Variable benar: $benar, variable salah: $salah";
+echo "Variable benar: $benar, variable salah: $salah <br>";
 
 // Mendefinisikan konstanta untuk nilai tetap 
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023); 
 
 echo "Selamat datang di". NAMA_SITUS . ", situs yang 
-didirikan pada tahun " . TAHUN_PENDIRIAN.".";
+didirikan pada tahun " . TAHUN_PENDIRIAN.".<br>";
 ?>
