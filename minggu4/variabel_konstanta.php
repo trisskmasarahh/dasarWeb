87071f5339967665f3db886b4633a5becfa11be6
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-19 08:00:54 +0700

    praktikum 1 variable konstanta

---
+++ minggu4/variabel_konstanta.php
@@ -0,0 +1,17 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasi = $angka1 +$angka2;
+echo"Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. ";
+
+$benar = true;
+$salah = false;
+echo "Variable benar: $benar, variable salah: $salah";
+
+// Mendefinisikan konstanta untuk nilai tetap 
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023); 
+
+echo "Selamat datang di". NAMA_SITUS . ", situs yang 
+didirikan pada tahun " . TAHUN_PENDIRIAN.".";
+?>
