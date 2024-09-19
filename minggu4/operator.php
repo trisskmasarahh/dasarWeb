8ff012cf7585253752612f0f22a773aca3b5d539
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-19 08:56:19 +0700

    praktikum 3 penggunaan operator PHP soal 3.1

---
+++ minggu4/operator.php
@@ -0,0 +1,16 @@
+<?php
+$a = 10;
+$b = 5;
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil penjumlahan $a + $b = $hasilTambah<br>";
+echo "Hasil pengurangan $a - $b = $hasilKurang<br>";
+echo "Hasil perkalian $a * $b = $hasilKali<br>";
+echo "Hasil pembagian $a / $b = $hasilBagi<br>";
+echo "Sisa bagi $a % $b = $sisaBagi<br>";
+echo "Hasil pemangkatan $a ^ $b = $pangkat<br>";
