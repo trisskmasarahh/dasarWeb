30b025163e35a279d9b7893f3c69790a2594d630
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-28 21:39:42 +0700

    perbaikan nomor 1

--- Praktikum_jquery/lanjutanPhp/fungsi.php
+++ Praktikum_jquery/lanjutanPhp/fungsi.php
@@ -12,17 +12,27 @@
 // $ucapanSalam = "Selamat Pagi";
 
 // perkenalan($saya);
-function hitungUmur($thnLahir, $thnHariIni) {
-    $umur = $thnHariIni - $thnLahir;
-    return $umur;
-}
-function perkenalan($nama, $salam="Asalamualaikum"){
-    echo $salam.",";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Umur saya adalah ".hitungUmur(2004, 2024)." tahun";
-}
-perkenalan("Sukma");
+// function hitungUmur($thnLahir, $thnHariIni) {
+//     $umur = $thnHariIni - $thnLahir;
+//     return $umur;
+// }
+// function perkenalan($nama, $salam="Asalamualaikum"){
+//     echo $salam.",";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Umur saya adalah ".hitungUmur(2004, 2024)." tahun";
+// }
+// perkenalan("Sukma");
 
 // echo "<br>";
 // echo "Umur saya adalah ".hitungUmur(2004, 2024)." tahun";
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Sarah<br>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br>";
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Sarah<br>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br>";
+}
+  // Memanggil fungsi yang sudah dibuat
+perkenalan();
 ?>
