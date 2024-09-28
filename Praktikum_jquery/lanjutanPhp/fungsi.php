b2af247b184ae98bf9334574297d59c4448beab2
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-28 20:59:27 +0700

    soal nomor 4

--- Praktikum_jquery/lanjutanPhp/fungsi.php
+++ Praktikum_jquery/lanjutanPhp/fungsi.php
@@ -1,15 +1,22 @@
 <?php
-function perkenalan($nama , $salam="Assalamualaikum"){
-    echo $salam.",";
-    echo "Perkenalkan, nama saya ".$nama." <br>";
-    echo "Senang berenalan dengan anda <br>";  
+// function perkenalan($nama , $salam="Assalamualaikum"){
+//     echo $salam.",";
+//     echo "Perkenalkan, nama saya ".$nama." <br>";
+//     echo "Senang berenalan dengan anda <br>";  
+// }
+// perkenalan("Sukma", "Selamat Pagi");
+
+// echo "<hr>";
+
+// $saya = "Sarah";
+// $ucapanSalam = "Selamat Pagi";
+
+// perkenalan($saya);
+function hitungUmur($thnLahir, $thnHariIni) {
+    $umur = $thnHariIni - $thnLahir;
+    return $umur;
 }
-perkenalan("Sukma", "Selamat Pagi");
 
-echo "<hr>";
-
-$saya = "Sarah";
-$ucapanSalam = "Selamat Pagi";
-
-perkenalan($saya);
+echo "<br>";
+echo "Umur saya adalah ".hitungUmur(2004, 2024)." tahun";
 ?>
