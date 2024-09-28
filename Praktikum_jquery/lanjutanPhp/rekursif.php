e3921d8506d77860e67135f13447c598cebc87fa
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-28 21:08:49 +0700

    soal nomor 6

--- Praktikum_jquery/lanjutanPhp/rekursif.php
+++ Praktikum_jquery/lanjutanPhp/rekursif.php
@@ -1,9 +1,18 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo Dunia <br>";
-    tampilkanHaloDunia();
+// function tampilkanHaloDunia(){
+//     echo "Halo Dunia <br>";
+//     tampilkanHaloDunia();
     
+// }
+// tampilkanHaloDunia();
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke={$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
-tampilkanHaloDunia();
+tampilkanAngka(20);
 
 ?>
