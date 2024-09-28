ec7cacde68c97b52bccdaa74c6c9b933b1f60008
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-28 20:40:24 +0700

    tugas no 3

--- Praktikum_jquery/lanjutanPhp/fungsi.php
+++ Praktikum_jquery/lanjutanPhp/fungsi.php
@@ -3,10 +3,12 @@ function perkenalan(){
     echo "Assalamualaikum, ";
     echo"Perkenalkan, nama saya sarah<br/>";
     echo"Senang berkenalan dengan Anda<br/>";
+
 }
 perkenalan("Hamdana", "Hallo");
 echo "<h>";
-$saya ="Elok";
-$uacapanSalam ="Selamat pagi";
-perkenalan($saya, $uacapanSalam);
+$saya = "Elok";
+$uacapanSalam = "Selamat pagi";
+// perkenalan($saya, $uacapanSalam);
+perkenalan($saya);
 ?>
