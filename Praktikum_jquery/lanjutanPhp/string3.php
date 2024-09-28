d2cc3521a2ab370b03a0ccace56ccd4122049f6a
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-09-28 21:33:24 +0700

    soal nomor 12

--- Praktikum_jquery/lanjutanPhp/string3.php
+++ Praktikum_jquery/lanjutanPhp/string3.php
@@ -1,4 +1,14 @@
 <?php
 $pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+
+// Ubah variabel $pesan menjadi array dengan perintah explode, memisahkan berdasarkan spasi
+$pesanPerKata = explode(" ", $pesan);
+
+// Ubah setiap kata dalam array menjadi kebalikannya menggunakan fungsi strrev dan array_map
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+
+// Gabungkan kembali array menjadi string dengan perintah implode
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 ?>
