7662ee33b52b8cc6508061f9f36796cdd2d63205
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-03 08:22:22 +0700

    pertemuan 6 _praktikum 1

---
+++ minggu6/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Array Terindeks</h2>
+<?php
+$Listdosen=["Sarah", "Sukma", "Dede"];
+
+echo $Listdosen[2] . "<br>";
+echo $Listdosen[0] . "<br>";
+echo $Listdosen[1] . "<br>";
+?>
+</body>
+</html>
