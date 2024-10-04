7fa82dc9a0f39e1e3b898cbf744ef748978ae10e
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-04 18:48:07 +0700

    soal nomor 10

--- minggu6/rekursif.php
+++ minggu6/rekursif.php
@@ -1,8 +1,21 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia!<br>";
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-" . $indeks . "<br>";
+
+    // Panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
 
-tampilkanHaloDunia();
-tampilkanHaloDunia();
+tampilkanAngka(20);
+// for ($i = 1; $i <= 25; $i++) {
+//     echo "Perulangan ke-" . $i . "<br>";
+// }
+// // function tampilkanHaloDunia() {
+//     echo "Halo dunia!<br>";
+// }
+
+// tampilkanHaloDunia();
+// tampilkanHaloDunia();
 ?>
