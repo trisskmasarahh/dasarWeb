ed51e097254d25de2323036b40caf84286b97dd3
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-04 18:23:41 +0700

    soal nomor 4

---
+++ minggu6/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Sarah<br>"; 
+    echo "Senang berkenalan dengan Anda<br>";
+}
+
+// Memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
