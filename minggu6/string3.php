9c65e9a4d0b9ae1cd6a09c3fdceca745a201df70
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-04 19:19:42 +0700

    soal nomor 16

--- minggu6/string3.php
+++ minggu6/string3.php
@@ -1,4 +1,16 @@
 <?php
-$pesan = "Saya arek malang";
-echo strrev($pesan). "<br>";
+$pesan = "saya arek malang";
+
+// Ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+
+// Ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+
+// Gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
+// $pesan = "Saya arek malang";
+// echo strrev($pesan). "<br>";
 ?>
