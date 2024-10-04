6d1c00364ce4823965762da80b995f1188714ae4
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-04 17:45:07 +0700

    soal nomor 2 praktikum 2

--- minggu6/array_2.php
+++ minggu6/array_2.php
@@ -3,7 +3,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Styled Table</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 50%;
+            margin: 20px auto;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 12px;
+            text-align: left;
+            border: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: blue;
+        }
+        tr:hover {
+            background-color: pink;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -12,10 +36,24 @@
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-
-    echo "Nama : " . $Dosen['nama'] . "<br>";
-    echo "Domisili : " . $Dosen['domisili'] . "<br>";
-    echo "Jenis Kelamin : " . $Dosen['jenis_kelamin'] . "<br>";
     ?>
+    <table>
+        <tr>
+            <th>Label</th>
+            <th>Detail</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 </html>
