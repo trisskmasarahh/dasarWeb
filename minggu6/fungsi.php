e97f5b60df127a42032855c70acf293ab0f3f41e
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-04 18:27:53 +0700

    soal nomor 6

--- minggu6/fungsi.php
+++ minggu6/fungsi.php
@@ -1,11 +1,28 @@
 <?php
-function perkenalan() {
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Sarah<br>"; 
+function perkenalan($nama, $salam) {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br>";
     echo "Senang berkenalan dengan Anda<br>";
 }
 
+// Memanggil fungsi dengan argumen
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+// Mendefinisikan variabel
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+// Memanggil fungsi dengan variabel
+perkenalan($saya, $ucapanSalam);
+// function perkenalan() {
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Sarah<br>"; 
+//     echo "Senang berkenalan dengan Anda<br>";
+// }
+
 // Memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+// perkenalan();
+// perkenalan();
 ?>
