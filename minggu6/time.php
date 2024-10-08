7384a1b5a4522dcf8518837671a01df1108e5f11
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-08 07:49:00 +0700

    soal nomor 20

---
+++ minggu6/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Time Example</title>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php 
+date_default_timezone_set("Asia/Jakarta"); 
+    echo date("h:i:sa"); 
+    ?>
+</body>
+</html>
