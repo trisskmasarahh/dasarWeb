9a793df621e708f57642ded3d3bdc298d5a69684
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-10 07:49:15 +0700

    praktikum 1 soal nomor 2

--- minggu7/isset.php
+++ minggu7/isset.php
@@ -5,5 +5,13 @@ if (isset($umur) && $umur  >= 18) {
 }else{
     echo "Anda belum dewasa atau variable 'Umur' tidak ditemukan.";
 }
+echo "<br>";
+$data = array("nama" => "Jane", "usia" => 25);
+
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
 
 ?>
