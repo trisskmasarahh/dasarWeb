7be15b67939ac1eba0250ea0894b5dc00ac5a8f8
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-16 17:52:23 +0700

    soal nomor 5.2 praktikum 5

--- minggu7/regex.php
+++ minggu7/regex.php
@@ -6,4 +6,13 @@ if (preg_match($pattern,$text)) {
 }else{
     echo "Tidak ada huruf kecil!";
 }
+echo"<br><br>";
+$pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: ". $matches[0];
+} else {
+    echo "Tidak ada yang cocok";
+} 
+
 ?>
