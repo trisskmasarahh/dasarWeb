28f73cb6c83a9743009658bbbf4fe41b8af3effd
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-17 09:18:23 +0700

    praktukum 8 soal nomor 3

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -2,12 +2,22 @@
 if (isset($_POST["submit"])) {
     $targetdir = "upload/";
     $targetfile = $targetdir.basename($_FILES["myfile"]["name"]);
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
         echo"File berhasil diunggah";
-        
-    }
-    else{
-        echo"Gagal mengunggah file";
+        } else{
+            echo"Gagal mengunggah file";
+        }
+    } else {
+        echo "file tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
+    
+
 ?>
