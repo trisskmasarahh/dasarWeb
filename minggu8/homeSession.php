21ced2ac5f0b2500aaa5af1df7ef76b36d22f9bf
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-31 00:17:44 +0700

    soal 22- 24

---
+++ minggu8/homeSession.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Halaman Home</title>
+    </head>
+    <body>
+        <?php
+            session_start();
+            if ($_SESSION['status'] =="login") {
+                echo "Selamat datang " . $_SESSION['username'];
+
+        ?>
+                <br>
+                <a href="sessionLogOut.php">Logout</a>
+        <?php
+            }
+            else {
+                echo "Anda belum login, silahkan";
+
+        ?>
+                <a href="sessionLoginForm.html">Login</a>
+        <?php
+            }
+        ?>
+    </body>
+</html>
