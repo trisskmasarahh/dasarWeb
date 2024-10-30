c5f562b47c9a05eab19d4de503d849fee61925fb
Tri Sukma Sarah <trisukmasarah206@gmail.com>
2024-10-30 22:17:40 +0700

    soal nomor 9

--- minggu8/proses_upload.php
+++ minggu8/proses_upload.php
@@ -1,6 +1,29 @@
 <?php
 $targetDirectory = "documents/";
 
+// if (!file_exists($targetDirectory)) {
+//     mkdir($targetDirectory, 0777, true);
+// }
+
+// if ($_FILES['files']['name'][0]) {
+//     $totalFiles = count($_FILES['files']['name']);
+
+//     for ($i=0; $i < $totalFiles; $i++) { 
+//         $fileName = $_FILES['files']['name'][$i];
+//         $targetFile = $targetDirectory . $fileName;
+
+//         if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+//             echo "File $fileName berhasil diunggah. <br>";
+//         } else {
+//             echo "Gagal mengunggah file $fileName. <br>";
+//         }   
+//     }
+// } else {
+//     echo "Tidak ada file yang diunggah.";
+// }
+$targetDirectory = "document_Gambar/";
+$allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
@@ -10,14 +33,20 @@ if ($_FILES['files']['name'][0]) {
 
     for ($i=0; $i < $totalFiles; $i++) { 
         $fileName = $_FILES['files']['name'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
         $targetFile = $targetDirectory . $fileName;
 
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah. <br>";
+        if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah. <br>";
+            } else {
+                echo "Gagal mengunggah file $fileName. <br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName. <br>";
-        }   
+            echo "File $fileName tidak valid atau memiliki tipe yang tidak diizinkan. <br>";
+        }
     }
 } else {
     echo "Tidak ada file yang diunggah.";
 }
+?>
